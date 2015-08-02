25421748e96df8395d56698be79e74b28b5425c5
Saad Ather <saadather04@gmail.com>
2015-08-02 12:48:42 -0500

    Project Setup

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://wirlzy1:Baigan@ds043348.mongolab.com:43348/wirlzylearning'
+  },
+
+  seedDB: true
+};
---
+++ server/config/environment/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Test specific configuration
+// ===========================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://wirlzy:Baigan@123@ds043348.mongolab.com:43348/wirlzylearning'
+  }
+};
